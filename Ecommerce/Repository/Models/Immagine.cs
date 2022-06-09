32b3c8c41393c4d813f86d3e8c99f367cafc361d
gbelda <gbelda56@gmail.com>
2022-06-09 18:14:17 +0200

    add files

---
+++ Ecommerce/Repository/Models/Immagine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ecommerce.Repository.Models
+{
+    public partial class Immagine
+    {
+        public int Id { get; set; }
+        public int IdProdotto { get; set; }
+        public string Link { get; set; } = null!;
+
+        public virtual Prodotto IdProdottoNavigation { get; set; } = null!;
+    }
+}
---
+++ Ecommerce/Services/ImmagineServices.cs
@@ -0,0 +1,38 @@
+﻿using Ecommerce.Repository;
+using Ecommerce.Repository.Models;
+
+namespace Ecommerce.Services
+{
+    public class ImmagineServices
+    {
+        private readonly ShopDbContext _dbContext;
+
+        public ImmagineServices(ShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Immagine> GetImgs()
+        {
+            List<Immagine> imgs = _dbContext.Immagines.ToList();
+            return imgs;
+        }
+
+        public void UploadImg()
+        {
+            Immagine IphoneImg = new Immagine();
+            IphoneImg.Id = 1;
+            IphoneImg.Link = "/img/iphone1.png";
+            IphoneImg.IdProdotto = 1;
+            _dbContext.Immagines.Add(IphoneImg);
+            _dbContext.SaveChanges();
+            Immagine IphoneImg2 = new Immagine();
+            IphoneImg2.Id = 2;
+            IphoneImg2.Link = "/img/iphone2.png";
+            IphoneImg2.IdProdotto = 1;
+            _dbContext.Immagines.Add(IphoneImg2);
+            _dbContext.SaveChanges();
+        }
+
+    }
+}
